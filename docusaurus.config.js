7117f0971ff17b4af354fd84a4a2b17db10c308f
hasanaliozkan-dev <hasanaliozkan@posta.mu.edu.tr>
2025-11-20 02:05:06 +0300

    night

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,9 +27,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'virtusrnd', // Usually your GitHub org/user name.
+  projectName: 'blog', // Usually your repo name.
+  deploymentBranch: 'gh-pages', 
   onBrokenLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
