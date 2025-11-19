af00ef17c5902b8257653393a9e65fe34c6031e3
hasanaliozkan-dev <hasanaliozkan@posta.mu.edu.tr>
2025-11-20 02:11:01 +0300

    Updates

---
+++ assets/js/868df6f0.617ffb12.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkblog=globalThis.webpackChunkblog||[]).push([[6091],{3923:l=>{l.exports=JSON.parse('{"metadata":{"permalink":"/blogs/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
